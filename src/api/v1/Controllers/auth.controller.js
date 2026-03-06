c4aabbd504b8763274797ba41c9475dc9d1510c7
Rajat97588 <rajatgandhi979@gmail.com>
2026-03-06 10:06:10 +0530

    Backend login API integrated

--- src/api/v1/Controllers/auth.controller.js
+++ src/api/v1/Controllers/auth.controller.js
@@ -1,3 +1,22 @@
+// import logger from '../../../helper/logger.js';
+
+// class AuthController {
+
+//   // ----------------------- LOGIN -----------------------
+//   async login(req, res) {
+//     try {
+
+
+//     } catch (error) {
+//       logger.error(error);
+//       return res.status(500).json({ message: "Internal Server Error" });
+//     }
+//   }
+// }
+
+// export default new AuthController();
+
+
 import logger from '../../../helper/logger.js';
 
 class AuthController {
@@ -6,10 +25,40 @@ class AuthController {
   async login(req, res) {
     try {
 
+      // get data from request body
+      const { email, password } = req.body;
+
+      // check if fields are empty
+      if (!email || !password) {
+        return res.status(400).json({
+          status: false,
+          message: "Email and Password are required"
+        });
+      }
+
+      // temporary login check (for testing)
+      if (email === "admin@gmail.com" && password === "123456") {
+        return res.status(200).json({
+          status: true,
+          message: "Login Successful",
+          user: {
+            email: email
+          }
+        });
+      }
+
+      // if credentials wrong
+      return res.status(401).json({
+        status: false,
+        message: "Invalid email or password"
+      });
 
     } catch (error) {
       logger.error(error);
-      return res.status(500).json({ message: "Internal Server Error" });
+      return res.status(500).json({
+        status: false,
+        message: "Internal Server Error"
+      });
     }
   }
 }
